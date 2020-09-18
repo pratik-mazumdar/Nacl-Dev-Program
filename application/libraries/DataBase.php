0dc5528e65b5f3f1f296dd59194dedde717599b4
Pratik Mazumdar <45979935+LUCIF680@users.noreply.github.com>
2020-09-18 06:44:07 +0530

    Staring web app added

--- application/libraries/DataBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-abstract class DataBase{
-	protected $dbname = "nacl";
-	protected $servername = "localhost";
-	protected $username_database = "root";
-	protected $password_database = "";
-	protected $driver = "mysql";
-}
